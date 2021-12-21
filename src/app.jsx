b5baf32f29dc300f484bf1fae0266f346770d877
wjddk0909 <fjddkcb@naver.com>
2021-12-21 22:42:12 +0900

    app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -2,48 +2,7 @@ import {useState} from 'react';
 import './app.css';
 
 function App() {
-
-  const [movieCont, setMovieCont] = useState({
-    title: '',
-    content: ''
-  })
-
-  const [viewCont, setViewCont] = useState([]);
-
-  const getValue = e => {
-    const {name, value} = e.target;
-    // console.log(name, value);
-    setMovieCont({
-      ...movieCont,
-      [name]: value
-    });
-    console.log(movieCont);
-  }
-
-  return (
-    <div className='App'>
-      <h1>영화 리뷰</h1>
-      <div className='form-wrap'>
-        <input type="text" className='tit-input' placeholder='영화 제목을 입력해주세요' onChange={getValue} name='title'/>
-        <textarea className='txt-area' placeholder='내용을 입력하세요'></textarea>
-      </div>
-      <button className='submit-btn'
-        onClick={() => {
-          setViewCont(viewCont.concat({...movieCont}));
-        }}
-      >저장</button>
-      <div className='movie-cont'>
-        {viewCont.map(element => 
-          <div className='movie-box'>
-            <h2>{element.title}</h2>
-            <div>
-              {HtmlReactParser(element.content)}
-            </div>
-          </div>
-        )}
-      </div>
-    </div>
-  );
+  return ('안녕하세요 만나서 반가워요');
 }
 
 export default App;
